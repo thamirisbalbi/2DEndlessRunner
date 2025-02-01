19909e40ab2aae72398d7752f562be1fcb70eca1
thamirisbalbi <thamirisbalbi2277@gmail.com>
2025-02-01 02:40:46 -0300

    ObjectPool e Spawner Controller

--- Assets/Scripts/SpawnerController.cs
+++ Assets/Scripts/SpawnerController.cs
@@ -4,26 +4,26 @@ public class SpawnerController : MonoBehaviour
 {
     public GameObject[] enemyPrefab;
     public ObjectPool enemyPool;
-    public float tempoDecorrido;
+    public float timePassed;
 
     void Start()
     {
 
         enemyPool = new ObjectPool(enemyPrefab, 6);
-        tempoDecorrido = 0;
+        timePassed = 0;
     }
 
 
     void Update()
     {
-        tempoDecorrido += Time.deltaTime;
-        if (tempoDecorrido >= 1)
+        timePassed += Time.deltaTime;
+        if (timePassed >= 1)
         {
             GameObject obj = enemyPool.GetFromPool();
             if (obj)
             {
                 obj.transform.position = transform.position;
-                tempoDecorrido = 0;
+                timePassed = 0;
             }
         }
     }
